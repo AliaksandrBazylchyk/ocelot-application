d62d1b2ddb220eeb7aedff32ddae0a12254cbd16
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-04-21 14:52:32 +0300

    Created example controller

---
+++ ocelot-securedApi/Controllers/SecuredController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ocelot_securedApi.Models;
+
+namespace ocelot_securedApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecuredController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Private information");
+        }
+
+        [HttpGet("{number:int}")]
+        public async Task<IActionResult> GetNumberSquare(int number)
+        {
+            return Ok($"Secured number square: {number * number}");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] NamesModel model)
+        {
+            var firstName = model.FirstName ?? throw new ArgumentNullException();
+            var lastName = model.LastName ?? throw new ArgumentNullException();
+
+            return Ok($"Your secured name: {firstName} {lastName}");
+        }
+    }
+}
---
+++ ocelot-securedApi/Models/NamesModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ocelot_securedApi.Models
+{
+    public class NamesModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
