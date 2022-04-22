e33c469d6e75f3fcce7f77d7c55db6fea5ea684e
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-04-22 16:35:07 +0300

    Deleted magic strings

---
+++ ocelot-common/HttpCodeErrorDescription.cs
@@ -0,0 +1,8 @@
+﻿namespace ocelot_common
+{
+    public static class HttpStatusErrorDescription
+    {
+        public static string Unauthorized = "Bad credentials";
+        public static string UnsupportedMediaType = "Wrong body";
+    }
+}
